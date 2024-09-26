1413e6f8ea6c7d01c6fe2b907ab4c522e986ca60
Haneul Kim <haneul.kim.dev@gmail.com>
2024-09-26 13:11:57 +0900

    Add compile-resources.bat script for aggregating and compiling resources

--- HighlevelAPIs/API_SOURCE/FIncludes.h
+++ HighlevelAPIs/API_SOURCE/FIncludes.h
@@ -50,6 +50,7 @@
 #include "generated/resources/gltf_demo.h"
 //#include "../../VisualStudio/libs/filamentapp/generated/resources/filamentapp.h"
 #include "materials/uberarchive.h"
+#include "generated/res/mat_internal.h"
 
 // FEngine
 #include "../../libs/gltfio/src/FFilamentAsset.h"
